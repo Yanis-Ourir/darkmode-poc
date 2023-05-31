157779ec39a4cad03c8658c2bbe9489f119666f7
Yanis-Ourir <ourir.yanis@gmail.com>
2023-05-31 16:18:00 +0200

    second commit

--- content.tsx
+++ content.tsx
@@ -1,117 +1,201 @@
 import { Storage } from "@plasmohq/storage";
+import { test } from "node:test";
 import React, { useEffect, useState } from "react";
 
 
 const styleDarkMode = document.createElement("style");
 const styleDarkHome = document.createElement("style");
 const styleDarkContact = document.createElement("style");
+const styleDarkActivity = document.createElement("style");
 
 export default function darkMode() {
-  const newColorBody = "rgba(4, 18, 26, 255)";
-  const newTextColor = "rgba(250, 250, 250)";
-  const testColor = "rgba(22,24,28,255)";
-  const blueColor = "rgba(0, 197, 255)";
+  // const darkColor = "rgba(4, 18, 26, 255)";
+  // const whiteColor = "rgba(250, 250, 250)";
+  // const greyColor = "rgba(22,24,28,255)";
+  // const blueColor = "rgba(0, 197, 255)";
 
-  // page après -> connexion
+  const classNames = Array.from(
+    Array.from(
+      document.querySelectorAll("[class]")
+    ).reduce(
+      (set, elements) => (elements.classList.forEach(c => set.add(c)), set),
+      new Set()
+    )
+  ).sort();
 
-  const darkModeStyles = `
-  nav {
-      background-color: ${testColor}!important;
-      color: ${newTextColor};
-    }
-    #hs-nav-v4 .primary-links>li.active>a, #hs-nav-v4 .primary-links>li.currentPage>a {
-      background-color: ${testColor}!important;
-    }
-    .navSearch-input {
-      background-color: ${testColor}!important;
-    }
-    .results-header header--full-width {
-      background-color: ${newColorBody}!important;
-    }
+  console.table(classNames);
 
-    body,
-    .private-template__section--stretch,
-    .private-header,
-    .private-button--tertiary,
-    .private-button--unstyled,
-    .col-xs-12,
-    .SetupGuideLayout__SetupGuideNavContainer-hatnox-1,
-    .UINavList__Ul-bzb0ye-0 {
-      background-color: ${newColorBody} !important;
-      color: ${newTextColor};
-    }
-
-    .dVdmzr {
-      background-color: transparent;
-    }
-    .ljQpdU {
-      background-color: ${newColorBody}!important;
-    }
-    .private-card__section {
-      background-color: ${testColor};
-      color: ${newTextColor};
-    }
+  function applyDarkMode(element) {
     
-   
-  `;
+    const textColorsToReplace = ['black', '#000', '#000000', '#33475b', 'rgb(51, 71, 91)'];
+    const backgroundColorsToReplace = ['white', '#FFF', '#FFFFFF', 'rgba(0, 0, 0, 0)', 'rgb(255, 255, 255)', 'rgb(81, 111, 144)', 'rgb(37, 51, 66)'];
 
-  styleDarkMode.innerHTML = darkModeStyles;
-  document.head.appendChild(styleDarkMode);
+    // Couleurs de remplacement pour le mode sombre
+    const darkTextColor = '#CCC';
+    const darkBackgroundColor = '#333';
 
-  //  page accueil 
-  const darkModeHome = `
-    .private-template__section--header {
-      background-color: ${newColorBody};
-    }
-    .private-button--secondary {
-      background-color: #ff7a59;
-      color: ${newTextColor};
-    }
-    .widget-card {
-      background-color: ${testColor};
-      color: ${newTextColor}
+    // Vérifie et modifie la couleur de texte
+    const textColor = window.getComputedStyle(element).color;
+    console.log("text" + " " + textColor);
+    if (textColorsToReplace.includes(textColor)) {
+      element.style.color = darkTextColor;
     }
 
-    .widget-card .private-card__section {
-      background-color: transparent !important;
+    // Vérifie et modifie la couleur de fond
+    const backgroundColor = window.getComputedStyle(element).backgroundColor;
+    console.log("background" + " " + backgroundColor)
+    if (backgroundColorsToReplace.includes(backgroundColor)) {
+      element.style.backgroundColor = darkBackgroundColor;
     }
 
-  `
-
-  styleDarkHome.innerHTML = darkModeHome;
-  document.head.appendChild(styleDarkHome);
-
-  //page contact 
-  const darkModeContact = `
-  
-  .gHnHIK {
-    color: ${newColorBody};
+    // Parcours les éléments enfants
+    
   }
-  
-  .gZHdNR {
-    background-color: ${testColor} !important;  
-  }
+  classNames.forEach(className => {
+    const classes = document.getElementsByClassName(className.toString()) as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < classes.length; i++) {
+      applyDarkMode(classes[i]);
+    }
+  })
 
-  .LbseA {
-    background-color: transparent;
-    border-color: ${blueColor};
-  }
 
-  .cBvQKy {
-    border-color: ${blueColor};
-  }
 
-  tr {
-    border-color: ${blueColor};
-  }
+  // console.table(classNames);
 
-  .gZHdNR thead th {
-    background-color: ${testColor};
-    color: ${newTextColor}
-  }
-  `
-  styleDarkContact.innerHTML = darkModeContact;
-  document.head.appendChild(styleDarkContact);
+  // classNames.forEach(className => {
+  //   const elements = document.getElementsByClassName(className.toString()) as HTMLCollectionOf<HTMLElement>;
+  //   for (let i = 0; i < elements.length; i++) {
+  //     elements[i].style.cssText = `background-color: ${darkColor}; color: ${whiteColor}`;
+  //   }
+  // });
+
+  // const darkModeStyles = `
+  // nav {
+  //     background-color: ${greyColor}!important;
+  //     color: ${whiteColor};
+  //   }
+  //   #hs-nav-v4 .primary-links>li.active>a, #hs-nav-v4 .primary-links>li.currentPage>a {
+  //     background-color: ${greyColor}!important;
+  //   }
+  //   .navSearch-input {
+  //     background-color: ${greyColor}!important;
+  //   }
+  //   .results-header header--full-width {
+  //     background-color: ${darkColor}!important;
+  //   }
+
+  //   .private-alert__title {
+  //     color: ${darkColor};
+  //   }
+
+  //   body,
+  //   .private-template__section--stretch,
+  //   .private-header,
+  //   .private-button--tertiary,
+  //   .private-button--unstyled,
+  //   .col-xs-12,
+  //   .SetupGuideLayout__SetupGuideNavContainer-hatnox-1,
+  //   .UINavList__Ul-bzb0ye-0 {
+  //     background-color: ${darkColor} !important;
+  //     color: ${whiteColor};
+  //   }
+
+  //   .dVdmzr {
+  //     background-color: transparent;
+  //   }
+  //   .ljQpdU {
+  //     background-color: ${darkColor}!important;
+  //   }
+  //   .private-card__section {
+  //     background-color: ${greyColor};
+  //     color: ${whiteColor};
+  //   }
+
+
+  // `;
+
+  // styleDarkMode.innerHTML = darkModeStyles;
+  // document.head.appendChild(styleDarkMode);
+
+  // //  page accueil 
+  // const darkModeHome = `
+  //   .private-template__section--header {
+  //     background-color: ${darkColor};
+  //   }
+  //   .private-button--secondary {
+  //     background-color: #ff7a59;
+  //     color: ${whiteColor};
+  //   }
+  //   .widget-card {
+  //     background-color: ${greyColor};
+  //     color: ${whiteColor}
+  //   }
+
+  //   .widget-card .private-card__section {
+  //     background-color: transparent !important;
+  //   }
+
+  // `
+
+  // styleDarkHome.innerHTML = darkModeHome;
+  // document.head.appendChild(styleDarkHome);
+
+  // //page contact 
+
+  // /*
+  //   gHnHIK = div barre de recherche au dessus du tableau
+  //   gZHdNR = th des tableaux 
+  //   LbseA = 
+
+
+
+  // */
+  // const darkModeContact = `
+
+  // .gHnHIK {
+  //   color: ${darkColor};
+  // }
+
+  // .gZHdNR {
+  //   background-color: ${greyColor} !important;  
+  // }
+
+  // .LbseA {
+  //   background-color: transparent;
+  // }
+
+
+  // tr {
+  //   border-color: ${blueColor};
+  // }
+
+  // .gZHdNR thead th {
+  //   background-color: ${greyColor};
+  //   color: ${whiteColor}
+  // }
+  // `
+  // styleDarkContact.innerHTML = darkModeContact;
+  // document.head.appendChild(styleDarkContact);
+  // /*
+  //  ikgKGw = div carte flux d'activité;
+  // */
+
+
+  // const darkModeActivity = `
+  //   .ikgKGw {
+  //     background-color: ${greyColor};
+  //   }
+
+  //   .private-selectable-box {
+  //     background-color: transparent !important;
+  //   }
+
+  //   .stream-notifications div {
+  //     // faire le changement de background color ici 
+  //   }
+  // `
+  // styleDarkActivity.innerHTML = darkModeActivity;
+  // document.head.appendChild(styleDarkActivity);
 }
 
 
--- popup.tsx
+++ popup.tsx
@@ -1,22 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./assets/styles.css"
 import { Storage } from "@plasmohq/storage"
 import darkMode from "~content";
+import getCurrentTab from "~functions/currentTab";
 
 function IndexPopup() {
 
+  const [toggle, setToggle] = useState(false);
 
-  function handleSubmit() {
-    // Activer la  fonction darkMode()
-  }
+  const executeContentScript = async () => {
+    const tab = await getCurrentTab();
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id, allFrames: true },
+      files: ["content.tsx"],
+    });
+  };
 
-
+  const handleToggle = () => {
+    setToggle(true);
+    console.log(toggle);
+    executeContentScript();
+  };
 
   return (
     <div>
-      <button type="submit" onSubmit={handleSubmit}>Dark mode</button>
+      <button type="button" onClick={handleToggle}>
+        Dark mode
+      </button>
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
